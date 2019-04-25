9ab5c688795237e2ecdbcf85401da9873a6c151b
EC2 Default User <ec2-user@ip-172-31-11-44.us-east-2.compute.internal>
2019-04-25 11:07:01 +0000

    works for multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,16 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns "buzz" when pass a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
